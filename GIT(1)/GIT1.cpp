71d195465d90178eb05435085e8480b043b58b9d
Ольга <@OLYA>
2018-05-05 09:42:09 +0300

    По батькові

--- GIT(1)/GIT1.cpp
+++ GIT(1)/GIT1.cpp
@@ -14,7 +14,8 @@ void main(void)
 	setlocale(LC_ALL, "UKR");
 	cout << "1. Прiзвище: Коцюбинська \n";
 	cout << "2.  Iм'я : Ольга \n";
-
+	cout << "3. По батькові : Вадимівна \n";
+	
 
 	system("pause");
 }
